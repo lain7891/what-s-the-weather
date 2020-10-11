1be54125a601da6ecd4c55865701fe1a9379509c
Estefania <nialupo87@gmail.com>
2020-10-11 18:58:54 -0400

    added an on click button

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+    
+
+    $("#search").on("click", function (event) {
+        event.preventDefault();
+    var city = $("#searchInput").val()
+    console.log(city);
+    });
 
 var apiKey = "905b9fba58e1578db8d708730effecf5";
 
-// moment().format('L');
 
- var searchCity = function(city){
+
+ 
          var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
          $.ajax({
              url: queryURL,
@@ -12,34 +19,18 @@ var apiKey = "905b9fba58e1578db8d708730effecf5";
            }).then(function(response) {
             //    console.log(queryURL);
                console.log(response);
-               console.log(searchCity);
+               
     
+        
           });
- }
+ 
 
-$("#city").on("click", function(event){
-     event.preventDefault();
 
-});
+//      searchCity("userInput");
+// });
 
  
 
 
-//     var city = $("#search").val();
-//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
-//            $.ajax({
-//             url: queryURL,
-//             method: "GET",
-//            }).then(function(response) {
-//                console.log(queryURL);
-//                console.log(response);
-//                $(".form-control").text(JSON.stringify(response));
 
-//         searchCity(response);
-//           });
-    
-//   });
-    
-
- searchCity("Atlanta");
 });
