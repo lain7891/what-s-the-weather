c8df752b636c762504fad4f37502f570e610b880
Estefania <nialupo87@gmail.com>
2020-10-11 19:08:53 -0400

    testing the onclick

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,25 @@
-$(document).ready(function(){
-    
-
-    $("#search").on("click", function (event) {
-        event.preventDefault();
-    var city = $("#searchInput").val()
+$(document).ready(function () {
+  $("#search").on("click", function (event) {
+    event.preventDefault();
+    var city = $("#searchInput").val();
     console.log(city);
-    });
+  });
 
-var apiKey = "905b9fba58e1578db8d708730effecf5";
+  var apiKey = "905b9fba58e1578db8d708730effecf5";
 
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    apiKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    //    console.log(queryURL);
+    console.log(response);
+  });
 
-
- 
-         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
-         $.ajax({
-             url: queryURL,
-             method: "GET",
-           }).then(function(response) {
-            //    console.log(queryURL);
-               console.log(response);
-               
-    
-        
-          });
- 
-
-
-//      searchCity("userInput");
-// });
-
- 
-
-
-
+  //      searchCity("userInput");
+  // });
 });
