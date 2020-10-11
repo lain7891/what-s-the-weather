07dca079ad3f462b472bc23062943d93b084899d
Estefania <nialupo87@gmail.com>
2020-10-11 16:58:55 -0400

    updating the ajax call

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,48 @@
 $(document).ready(function(){
 
-var searchCity = function(city){
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city-name + "&appid=905b9fba58e1578db8d708730effecf5";
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-      }).then(function(response) {
-          console.log(response);
-          searchCity(response);
-      });
+var apiKey = "905b9fba58e1578db8d708730effecf5";
 
-}
+// moment().format('L');
+
+ var searchCity = function(city){
+         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+         $.ajax({
+             url: queryURL,
+             method: "GET",
+           }).then(function(response) {
+            //    console.log(queryURL);
+               console.log(response);
+               console.log(searchCity);
+    
+         //   searchCity(response);
+          });
+
+        //   $(".btn").on(function(event){
+        //     event.preventDefault();
+
+        //   });
+
+ }
 
 
-searchCity("Atlanta");
-searchCity("New York");
-searchCity("Los Angelos");
+// $(".btn btn-primary").on("click", function(event){
+//     event.preventDefault();
+
+//     var city = $("#search").val();
+//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+//            $.ajax({
+//             url: queryURL,
+//             method: "GET",
+//            }).then(function(response) {
+//                console.log(queryURL);
+//                console.log(response);
+//                $(".form-control").text(JSON.stringify(response));
+
+//         searchCity(response);
+//           });
+    
+//   });
+    
+
+//  searchCity("Atlanta");
 });
